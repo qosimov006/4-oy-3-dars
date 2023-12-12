a01fdf2d3218936a0c1219c21da98384f4e5e990
qsosimov006 <mirjalolq274@gmail.com>
2023-12-12 17:31:38 +0500

    4-dars codlari

---
+++ app.js
@@ -0,0 +1,72 @@
+
+// 1-misol
+
+/*
+let juftSon = prompt("Iltimos, juft son kiriting:");
+let karraliSon = prompt("Iltimos, 5 ga karrali son kiriting:");
+
+if (juftSon % 2 === 0 && karraliSon % 5 === 0) {
+    let natija = juftSon * karraliSon;
+    console.log("Juft sonlar ko'paytmasi:", natija);
+} 
+else {
+   
+    console.log("Juft son yoki 5 ga karrali son emas!");
+}
+*/
+
+
+// 2-misol
+
+/*
+let son = prompt("Soni kiriting iltimosh")
+
+if(son % 3 == 0 && son % 7 ==0 ){
+    alert("Bu sonlar 3 ga ham 7 ga ham bo'linadi")
+}
+else{
+    alert(" Bu sonlar 3 ga ham 7 ga ham bo'linmaydi")
+}
+*/
+
+// 3-misol
+/*
+let son = prompt("Iltimos, 2 xonali son kiriting:");
+
+ let onlar = Math.floor(son / 10);
+let birlar = son %10;
+
+let yigindi = onlar +birlar
+
+console.log("Ikkixonali sonlar yig'indi shu",  yigindi)
+*/
+
+
+
+//5-misol
+/*
+let son = prompt("Iltimos, 3 xonali son kiriting:");
+
+ let yuzlar = Math.floor(son / 100);
+ let onlar = Math.floor((son % 100) / 10);
+
+let yigindi = onlar +yuzlar
+
+console.log("Ikkixonali sonlar yig'indi shu",  yigindi)
+*/
+
+// 8-misol
+
+/*
+let a = prompt("To'g'ri to'rtburchakning birinchi tomonini kiriting:");
+let b = prompt("To'g'ri to'rtburchakning ikkinchi tomonini kiriting:");
+
+let S = a * b;
+let P = 2 * (a + b);
+
+console.log("To'g'ri to'rtburchakning yuzasi:", S);
+console.log("To'g'ri to'rtburchakning perimetri:", P);
+*/
+
+
+
